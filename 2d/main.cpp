15180a89b1656e77f15c7a5b178e42b3ec07477c
oliegjio <oliegjio@gmail.com>
2018-10-24 08:52:54 +0000

    Initial Commit

---
+++ 2d/main.cpp
@@ -0,0 +1,63 @@
+#include <GL/glut.h>
+
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+
+clock_t current_time = clock();
+clock_t last_time = current_time;
+float dt = 0;
+
+void setup()
+{
+
+}
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+    glutSwapBuffers();
+}
+
+void reshape(int width, int height)
+{
+    glViewport(0, 0, width, height);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0f, WIN_WIDTH, 0.0f, WIN_HEIGHT, -1.0f, 1.0f);
+    glMatrixMode(GL_MODELVIEW);
+
+    glutPostRedisplay();
+}
+
+void idle()
+{
+	current_time = clock();
+	dt = static_cast<float>(current_time - last_time) / CLOCKS_PER_SEC;
+	last_time = current_time;
+	glutPostRedisplay();
+}
+
+int main(int argc, char **argv)
+{
+    setup();
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+    glutInitWindowPosition(0, 0);
+    glutInitWindowSize(WIN_WIDTH, WIN_HEIGHT);
+    glutCreateWindow("Window Title");
+
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+    glutDisplayFunc(display);
+    glutIdleFunc(idle);
+    glutReshapeFunc(reshape);
+
+    glutMainLoop();
+
+    return 0;
+}
---
+++ 3d/main.cpp
@@ -0,0 +1,72 @@
+#include <GL/glut.h>
+
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+
+clock_t current_time = clock();
+clock_t last_time = current_time;
+float dt = 0;
+
+void setup()
+{
+
+}
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glutSwapBuffers();
+}
+
+void reshape(int width, int height)
+{
+	glViewport(0, 0, width, height);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0f, (GLfloat) width / (GLfloat) height, 0.1f, 100.0f);
+	glMatrixMode(GL_MODELVIEW);
+
+	glutPostRedisplay();
+}
+
+void idle()
+{
+	current_time = clock();
+	dt = static_cast<float>(current_time - last_time) / CLOCKS_PER_SEC;
+	last_time = current_time;
+	glutPostRedisplay();
+}
+
+int main(int argc, char **argv)
+{
+	setup();
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(0, 0);
+	glutInitWindowSize(WIN_WIDTH, WIN_HEIGHT);
+	glutCreateWindow("Window Title");
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_COLOR_MATERIAL);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glDepthFunc(GL_LESS);
+	glShadeModel(GL_SMOOTH);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+	const float light_position[4] = { 0.0f, 0.0f, 10.0f, 1.0f };
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+	glClearDepth(1.0f);
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	glutDisplayFunc(display);
+	glutIdleFunc(idle);
+	glutReshapeFunc(reshape);
+
+	glutMainLoop();
+
+    return 0;
+}
